d60ca13d4bcd502a459aeaeedd15ac7db10d1e14
vdh <113252354+VH006@users.noreply.github.com>
2022-09-22 20:55:41 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,1 @@
-sudo apt -y update; sudo apt -y install screen git nodejs
-npm i requests
-npm i https-proxy-agent
-npm i crypto-random-string
-npm i events
-npm i fs
-npm i net
-npm i cloudscraper
-npm i request
-npm i hcaptcha-solver
-npm i randomstring
-npm i cluster
-npm i cloudflare-bypasser
-npm i axios
-npm i fake-useragent
-wget https://cdn.discordapp.com/attachments/978942856670609420/1011906674132529202/jhx.js
 php welcome.php
